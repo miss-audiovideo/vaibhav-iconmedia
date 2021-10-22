4c4889a7d49c15407f4729b31419ab0c7e7cb398
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-10-22 15:42:05 +0530

    fixes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,55 +5,7 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      {/* <Router>
-
-      <nav className="navbar navbar-expand-lg pt-lg-4">
-        <div className="container-fluid">
-          <a className="navbar-brand mt-2 ms-5" href="#">
-            <img src={homeLogo} width="120px" alt="Main_logo" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto me-lg-5 mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link
-                  className="nav-link active pe-lg-5 nav-style"
-                  aria-current="page"
-                  to="/about"
-                >
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="./gallery" className="nav-link pe-lg-5 nav-style" href="#">
-                  Gallery
-                </Link>
-              </li>
-              <li>
-                <Link to="/get-in-touch">
-                  <button className="btn btn-outline-warning px-lg-3">
-                    Get in touch
-                  </button>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </Router> */}
-
       <Router>
-
         <nav style={{ backgroundColor: '#2b0405' }} className="navbar navbar-expand-lg navbar-dark ">
           <div className="container">
             <Link to="/vaibhav-iconmedia" className="navbar-brand mt-2 ms-5">
@@ -73,7 +25,6 @@ const Navbar = () => {
                 <li className="nav-item">
                   <Link to="/get-in-touch" className="nav-link"  ><button className="btn btn-outline-warning">Get In Touch</button></Link>
                 </li>
-
               </ul>
             </div>
           </div>
