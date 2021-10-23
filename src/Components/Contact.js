9fa710e2ce48599db547a3a581c9e7a18ecbcc44
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-10-23 23:14:45 +0530

    fixes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,114 +1,97 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import "../app.css";
+import { db } from "../firebase";
 
-class Contact extends Component {
-    constructor(props) {
-        super(props)
+const Contact = () => {
+    const [email, setEmail] = useState("");
+    const [first, setNameFirst] = useState("");
+    const [last, setNameLast] = useState("");
+    const [interest, setInterest] = useState("");
+    const [price, setPrice] = useState("");
+    const [number, setNumber] = useState("");
+    const [addClass, setState] = useState("")
+    // const [loader, setLoader] = useState(false);
 
-        this.state = {
-            email: '',
-            first: '',
-            last: '',
-            interest: '',
-            price: '',
-            number: '',
-            addClass: false
-        }
+    const toggle = (e) => {
+        setState({ addClass: !addClass })
     }
-    toggle() {
-        this.setState({addClass: !this.state.addClass});
+    let boxClass = ["js-hiddenform"];
+    if (addClass) {
+        boxClass.push('js-form');
     }
-    
-    handleEmailChange = (event) => {
-        this.setState({
-            email: event.target.value
-        })
-        // $(".js-expand").on('click',function () {
-        //     if ($('#email').val()) {
-        //         //validate form
-        //     } else {
-        //         $(".js-hiddenform").slideDown();
-        //     }
-        // });
-    }
-    handleFirstChange = (event) => {
-        this.setState({
-            first: event.target.value
-        })
-    }
-    handleLastChange = (event) => {
-        this.setState({
-            last: event.target.value
-        })
-    }
-    handleInterestChange = (event) => {
-        this.setState({
-            interest: event.target.value
-        })
-    }
-    handlePriceChange = (event) => {
-        this.setState({
-            price: event.target.value
-        })
-    }
-    handleNumberChange = (event) => {
-        this.setState({
-            number: event.target.value
-        })
-    }
-    handleSubmit = (event) => {
-        alert(`${this.state.email}`)
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // setLoader(true);
 
+        db.collection("contacts")
+            .add({
+                email: email,
+                first: first,
+                last: last,
+                interest: interest,
+                price: price,
+                number: number,
+            })
+            .then(() => {
+                // setLoader(false);
+                alert("Your message has been submitted👍");
+            })
+            .catch((error) => {
+                alert(error.message);
+                // setLoader(false);
+            });
 
-    render() {
-        let boxClass = ["js-hiddenform"];
-        if(this.state.addClass) {
-            boxClass.push('js-form');
-        }
-        return (
-            <div className="email">
-                <h2 className="section-title">Contact Us</h2>
-                <div className="form-container">
-                    <form id="email-form" onSubmit={this.handleSubmit}>
-                        <input type="email" id="email" size="50" value={this.state.email} className="form-field js-expand" onChange={this.handleEmailChange} onClick={this.toggle.bind(this)} placeholder="Enter your email address" />
-                        <div className={boxClass.join(' ')}  >
-                            <div className="half-width">
-                                <label className="label" htmlFor="first">First Name</label>
-                                <input type="text" id="first" size="30" value={this.state.first} onChange={this.handleFirstChange} required="" className="form-field" placeholder="First Name" />
-                            </div>
-                            <div className="half-width">
-                                <label className="label" htmlFor="last">Last Name</label>
-                                <input type="text" id="last" size="30" value={this.state.last} onChange={this.handleLastChange} required="" className="form-field" placeholder="Last Name" />
-                            </div>
-                            <div className="clear"></div>
-                            <label className="label" htmlFor="interest">Requirements </label>
-                            <select id="interest" value={this.state.interest} onChange={this.handleInterestChange} size="1" required="" className="form-field">
-                                <option disabled="" value="selected">Select Your Needs</option>
-                                <optgroup label="Front End:">
-                                    <option value="GMAT">GMAT</option>
-                                </optgroup>
-                                <optgroup label="Back End:">
-                                    <option value="SAT">SAT</option>
-                                </optgroup>
-                                <optgroup label="Other:">
-                                    <option value="UNKNOWN">Don't Know</option>
-                                </optgroup>
-                            </select>
-                            <div className="half-width">
-                                <label className="label" htmlFor="price">Price</label>
-                                <input type="text" id="price" size="" required="" value={this.state.price} onChange={this.handlePriceChange} className=" form-field" placeholder="Price" />
-                            </div>
-                            <div className="half-width">
-                                <label className="label" htmlFor="number">Contact No.</label>
-                                <input type="text" id="number" size="" required="" maxLength="13" value={this.state.number} onChange={this.handleNumberChange} className=" form-field" placeholder="Contact Number" />
-                            </div>
-                            <div className="clear"></div>
+        setEmail("");
+        setNameFirst("");
+        setNameLast("");
+        setInterest("");
+        setPrice("");
+        setNumber("");
+    }
+    return (
+        <div className="email">
+            <h2 className="section-title">Contact Us</h2>
+            <div className="form-container">
+                <form id="email-form" onSubmit={handleSubmit}>
+                    <input type="email" id="email" size="50" value={email} className="form-field js-expand" onChange={(e) => setEmail(e.target.value)} onClick={toggle.bind(this)} placeholder="Enter your email address" />
+                    <div className={boxClass.join(' ')}>
+                        <div className="half-width">
+                            <label className="label" htmlFor="first">First Name</label>
+                            <input type="text" id="first" size="30" value={first} onChange={(e) => setNameFirst(e.target.value)} required="" className="form-field" placeholder="First Name" />
                         </div>
-                        <button id="submit" className="submit js-expand" type="submit" value="Send!">Submit</button>
-                    </form>
-                </div>
+                        <div className="half-width">
+                            <label className="label" htmlFor="last">Last Name</label>
+                            <input type="text" id="last" size="30" value={last} onChange={(e) => setNameLast(e.target.value)} required="" className="form-field" placeholder="Last Name" />
+                        </div>
+                        <div className="clear"></div>
+                        <label className="label" htmlFor="interest">Requirements </label>
+                        <select id="interest" value={interest} onChange={(e) => setInterest(e.target.value)} size="1" required="" className="form-field">
+                            <option disabled="" value="selected">Select Your Needs</option>
+                            <optgroup label="Front End:">
+                                <option value="GMAT">GMAT</option>
+                            </optgroup>
+                            <optgroup label="Back End:">
+                                <option value="SAT">SAT</option>
+                            </optgroup>
+                            <optgroup label="Other:">
+                                <option value="UNKNOWN">Don't Know</option>
+                            </optgroup>
+                        </select>
+                        <div className="half-width">
+                            <label className="label" htmlFor="price">Price</label>
+                            <input type="text" id="price" size="" required="" value={price} onChange={(e) => setPrice(e.target.value)} className=" form-field" placeholder="Price" />
+                        </div>
+                        <div className="half-width">
+                            <label className="label" htmlFor="number">Contact No.</label>
+                            <input type="text" id="number" size="" required="" maxLength="13" value={number} onChange={(e) => setNumber(e.target.value)} className=" form-field" placeholder="Contact Number" />
+                        </div>
+                        <div className="clear"></div>
+                    </div>
+                    <button id="submit" className="submit js-expand" type="submit" value="Send!">Submit</button>
+                </form>
             </div>
-        );
-    }
-}
-export default Contact
+        </div>
+    );
+};
+
+export default Contact;
