3b3d30f4c23b2612c26dd37e5d5a92171b00cb27
Swati Mandal <92773616+miss-audiovideo@users.noreply.github.com>
2021-10-25 22:29:06 +0530

    done

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
             <HeadingContent class="text-center text-capitalize display-2 w-50 mx-auto headingContent animate__animated animate__fadeInUp">
               <span className="heading-contentOne">GET THE</span>
               <span className="heading-contentTwo"> ATTENTION</span>
-              <br />
+            <br/>
               <span className="heading-contentOne">YOUR BRAND</span>
               <span className="heading-contentTwo"> DESERVES</span>
             </HeadingContent>
