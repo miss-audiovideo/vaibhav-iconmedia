df0ca4e73530b9ed341fb743b1a2744fe613c0d1
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-10-26 13:27:27 +0530

    cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -33,7 +33,7 @@ const Cards = (props) => {
                 width: "100%",
                 height: "100%",
                 objectFit: "cover",
-               
+              
               }}
             />
           }
