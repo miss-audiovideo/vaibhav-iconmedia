6390857b7734757a69577426c49bba40997e3272
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-10-28 23:31:59 +0530

    newdash

--- src/Components/NewDash.js
+++ src/Components/NewDash.js
@@ -1,108 +1,75 @@
 import React, { useState, useEffect } from "react";
 import "../Components/NewDash.css";
-import one from "../Components/images/girl.jpg";
-import two from "../Components/images/five.jpg";
-import three from "../Components/images/can.jpg";
+import Logo from "./dash/logo";
+import MarketMaterials from "./dash/market";
+import ContentCreation from "./dash/contentCreation";
+import DigitalAds from "./dash/digiAds";
+import CustomGraphics from "./dash/customGraphinc";
+import MotionGraphics from "./dash/motion";
+import WebsiteDevelopment from "./dash/webDev";
+import MobileApp from "./dash/mobileApp";
 
-const NewDash = () => {
-  const [addClass, setState] = useState("");
-  const toggle = (e) => {
-    setState({ addClass: !addClass });
-  };
-  let boxClass = one;
-  if (addClass) {
-    boxClass = two;
-  }
+function NewDash() {
+  const [id,setState] = useState("");
+  useEffect(() => {
+    setState("1");
+  }, []);
   return (
-<>
+  <>
     <div className="NewDash ">
       <div className="NewDash-Left">
       <div className="NewDash-text">
           <h3 className="NewDash-btn">
-          <button className="btn color-text" onClick={toggle.bind(this)}>
+          <button className="btn-new color-text" onClick={()=>setState("1")}>
             Logo
           </button>
           </h3>
           <h3 className="NewDash-btn">
-          <button className="btn color-text" onClick={toggle.bind(this)}>
+          <button className="btn-new color-text" onClick={()=>setState("2")}>
             Market Materials
           </button>
           </h3>
           <h3 className="NewDash-btn">
-          <button className="btn color-text" onClick={toggle.bind(this)}>
+          <button className="btn-new color-text" onClick={()=>setState("3")}>
             Content Creation
           </button>
           </h3>
           <h3 className="NewDash-btn">
-          <button className="btn color-text" onClick={toggle.bind(this)}>
+          <button className="btn-new color-text" onClick={()=>setState("4")}>
             Digital Ads
           </button>
           </h3>
           <h3 className="NewDash-btn">
-          <button className="btn color-text" onClick={toggle.bind(this)}>
+          <button className="btn-new color-text" onClick={()=>setState("5")}>
             Custom Graphics
           </button>
           </h3>
           <h3 className="NewDash-btn">
-          <button className="btn color-text" onClick={toggle.bind(this)}>
+          <button className="btn-new color-text" onClick={()=>setState("6")}>
             Motion Graphics
           </button>
           </h3>
           <h3 className="NewDash-btn">
-          <button className="btn color-text" onClick={toggle.bind(this)}>
+          <button className="btn-new color-text" onClick={()=>setState("7")}>
             Website Development
           </button>
           </h3>
           <h3 className="NewDash-btn">
-          <button className="btn color-text" onClick={toggle.bind(this)}>
+          <button className="btn-new color-text" onClick={()=>setState("8")}>
             Mobile App
           </button>
           </h3>
         </div>
       </div>
       <div className="NewDash-Right">
-      <img
-            className="NewDash-img"
-            src={boxClass}
-            alt=""
-            width="250px"
-            height="250px"
-          />
-          <img
-            className="NewDash-img"
-            src={boxClass}
-            alt=""
-            width="250px"
-            height="250px"
-          />
-          <img
-            className="NewDash-img"
-            src={boxClass}
-            alt=""
-            width="250px"
-            height="250px"
-          />
-          <img
-            className="NewDash-img"
-            src={boxClass}
-            alt=""
-            width="250px"
-            height="250px"
-          />
-          <img
-            className="NewDash-img"
-            src={boxClass}
-            alt=""
-            width="250px"
-            height="250px"
-          />
-          <img
-            className="NewDash-img"
-            src={boxClass}
-            alt=""
-            width="250px"
-            height="250px"
-          />
+      {id === "1" && <Logo/> }
+      {id === "2" && <MarketMaterials/> }
+      {id === "3" && <ContentCreation/> }
+      {id === "4" && <DigitalAds/> }
+      {id === "5" && <CustomGraphics/> }
+      {id === "6" && <MotionGraphics/> }
+      {id === "7" && <WebsiteDevelopment/> }
+      {id === "8" && <MobileApp/> }
       </div>
     </div></>
   );
