f379eceacdb929a9adc774cbf6f8e374b92f6d1c
tejasksaindane <72094501+tejasksaindane@users.noreply.github.com>
2021-10-30 18:42:05 +0530

    quotes

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Buttons";
 import Image from "./images/c.png";
-function Quote() {
+import { Modal } from "react-bootstrap";
+const Quote = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <div className="quote animate__animated animate__bounce animate__delay-3s">
@@ -10,12 +12,40 @@ function Quote() {
           <br />
           impact on your business.
         </h1>
-        <Button
+        <button
+          onClick={() => setOpen(true)}
           className="quote-button btn btn-outline-warning"
           style={{ fontFamily: "Futura-Medium,sans-serif" }}
         >
           Design a quote
-        </Button>
+        </button>
+        <Modal
+          show={open}
+          className="animate__animated animate__fadeInBottomRight animate-slower animate__delay-1s"
+        >
+          <Modal.Header className="modal-head text-center"></Modal.Header>
+          <Modal.Body>
+            <center>
+              <h1 className="modal-head animate__animated animate__fadeInUp animate__delay-1s">
+                "Design A Quote"
+              </h1>
+              <h3>What services would you like to option ?</h3>
+              <p className="modal-p">UI/UX</p>
+              <p className="modal-p">Branding</p>
+              <p className="modal-p">Developement</p>
+              <p className="modal-p">Marketing</p>
+              <p className="modal-p">All Services</p>
+            </center>
+          </Modal.Body>
+          <Modal.Footer>
+            {/* <button onClick={() => setOpen(false)}>close Modal</button> */}
+            <i
+              class="fa fa-times cross"
+              aria-hidden="true"
+              onClick={() => setOpen(false)}
+            ></i>
+          </Modal.Footer>
+        </Modal>
       </div>
       <style jsx>{`
         @font-face {
@@ -25,6 +55,28 @@ function Quote() {
         @font-face {
           font-family: futurabook;
           src: url("./fonts/Futura\ Book.ttf");
+        }
+        .modal-p {
+          padding: 10px;
+          font-size: 24px;
+        }
+        .modal-p:hover {
+          background: #b2bec3;
+        }
+        .modal-head {
+          // display: flex;
+          // text-align: center;
+          color: red;
+        }
+        .cross {
+          font-size: 20px;
+          color: red;
+        }
+        .cross:hover {
+          background: red;
+          color: white;
+          padding: 15px;
+          border-radius: 50%;
         }
         .quote h1 {
           color: #ffe462;
@@ -66,6 +118,7 @@ function Quote() {
         }
         .quotes-button {
         }
+
         @media screen and (max-width: 500px) {
           .quote h1 {
             font-size: 35px;
@@ -74,6 +127,6 @@ function Quote() {
       `}</style>
     </>
   );
-}
+};
 
 export default Quote;
