ef91c679a01b49808f7434d8e779c8bdf204847b
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-13 17:55:01 +0530

    mid heading 2

--- src/Components/MidHeading.js
+++ src/Components/MidHeading.js
@@ -1,52 +1,56 @@
 import React from "react";
-import robo from "./images/Robo-1.gif"
+import robo from "./images/robo-2.gif"
 
 const MidHeading = () => {
     return (
-        <div className="mid-heading">Hi</div>
-        // <>
+        <>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+            <div className="mid-heading">
+                <div className="mid-heading-text">
+                    <div>Designs that make a difference</div>
+                </div>
+                <img src={robo} alt="robo" className="robo2"/>
+            </div>
+            <style jsx>{`
+            .mid-heading{
+                padding: 10%;
+                position: relative;
+            }
+                .mid-heading-text {
+                    border-radius: 10px;
+                    border: 3px solid #fff;
+                    font-family: "futuramedium";
+                    text-transform: uppercase;
+                    color: #ffc857 !important;
+                    font-size: 35pt;
+                }
+
+                .mid-heading-text div {
+                    padding: 10% 59% 10% 5%;
+                }
+                .robo2 {
+                    position: absolute;
+                    top: 0px;
+                    right: 150px;
+                    width: auto;
+                    height: 800px;
+                }
+            `}</style>
+        </>
+        // 
         //     <div className="luxury-heading">
         //         <div className="luxury">Luxury<span>of</span></div>
         //         <div className="design">Design</div>
         //     </div>
         //     <img src={robo} alt="robo" className="robo" />
-        //     <style jsx>{`
-        //     .luxury-heading{
-        //         text-align: center;
-        //     }
-        //         .luxury {
-        //             position: relative;
-        //             font-family: "cinzeldecorativebold";
-        //             color: #eaeaf0 !important;
-        //             font-size: 100pt;
-        //             text-shadow: 0px 0px 13px #eaeaf025;
-        //         }
-        //         .luxury span {
-        //             font-family: "herculesitalic";
-        //             color: #fff !important;
-        //             font-size: 30pt;
-        //         }
-
-        //         .design {
-        //             position: absolute;
-        //             top: 200px;
-        //             left: 500px;
-        //             text-align: center;
-        //             font-family: "cinzelbold";
-        //             color: #ffc857 !important;
-        //             font-size: 100pt;
-        //             text-shadow: 0px 0px 13px #ffc85725;
-        //         }
-        //         .robo {
-        //             width: 735px;
-        //             position: absolute;
-        //             top: 160px;
-        //             left: 100px;
-        //             height: auto;
-        //             -webkit-transform: scaleX(-1);
-        //             transform: scaleX(-1);
-        //         }
-        //     `}</style>
+        
         // </>
     );
 };
