b15788046d8bbf46db28ee11ebfed86349b9d945
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-20 17:32:35 +0530

    video

--- src/Components/360.js
+++ src/Components/360.js
@@ -1,27 +1,51 @@
 import React, {useRef, useState} from 'react';
 import video from './Video/360.mp4';
 
-const ThreeSixty = () => {
-    const [btn, setBtn] = useState("btn-icon");
-    const vidRef = useRef(null);
 
-    const handlePlayVideo = () => {
-        if(btn === "btn-icon"){
-            setBtn("btn-icon2");
-            vidRef.current.pause();
-        }else{
-            setBtn("btn-icon");
-            vidRef.current.play();
-        }
+const ThreeSixty = () => {
+    // function for hover play video
+    const videoRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(true);
+    const handleMouseOver = () => {
+        setIsPlaying(false);
+        videoRef.current.pause();
+    }
+    const handleMouseOut = () => {
+        setIsPlaying(true);
+        videoRef.current.play();
     }
     return (
         <div className="three-sixty-section">
-            <video muted autoPlay loop className="three-sixty-video">
-                <source src={video} type="video/mp4" />
-            </video>
-            <button className={btn + ` pause-btn`} onClick={handlePlayVideo}>+</button>
+                <video ref={videoRef} autoPlay={isPlaying} className="three-sixty-video" loop muted>
+                    <source src={video} type="video/mp4" />
+                </video>
+            <div className="three-sixty-btn">
+                <div className="btn-icon" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
+            </div>
         </div>
-    );
+    )
 }
+
+//     const handlePlayVideo = () => {
+//         if(videojs.getPlayers()["360"]) {
+//             setBtn("btn-icon2");
+//             vidRef.current.pause();
+//         }else{
+//             setBtn("btn-icon");
+//             vidRef.current.play();
+//         }
+//     }
+//     return (
+//         <div className="three-sixty-section">
+//             <video muted autoPlay loop className="three-sixty-video" onMouseOver={handlePlayVideo}>
+//                 <source src={video} type="video/mp4" />
+//             </video>
+//             <div className="three-sixty-overlay">
+//                 <div>360degree</div>
+//                 <button className="btn-icon">+</button>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default ThreeSixty;
