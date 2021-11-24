1e6520a73ef15c01af691692b3346a326b419a3e
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-24 20:26:24 +0530

    animation

--- src/Components/MidHeading.js
+++ src/Components/MidHeading.js
@@ -1,28 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useRef } from "react";
+import { useIntersection } from "react-use";
 import robo from "./images/robo-2.gif";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import gsap from "gsap";
 
 const MidHeading = () => {
-  useEffect(() => {
-    AOS.init({
-      offset: 400,
-      duration: 2000,
-      delay: 200,
-    });
+  // Ref for our element
+  const sectionRef = useRef(null);
+  // All the ref to be observed
+  const intersection = useIntersection(sectionRef, {
+    root: null,
+    rootMargin: "-280px",
+    threshold: 0.2
   });
+
+  // Animation for fading in
+  const fadeIn = element => {
+    gsap.to(element, 1, {
+      opacity: 1,
+      y: -60,
+      ease: "power4.out",
+      stagger: {
+        amount: 0.3
+      }
+    });
+  };
+  // Animation for fading out
+  const fadeOut = element => {
+    gsap.to(element, 1, {
+      opacity: 0,
+      y: -20,
+      ease: "power4.out"
+    });
+  };
+
+  // checking to see when the vieport is visible to the user
+  intersection && intersection.intersectionRatio < 0.2
+    ? fadeOut(".fadeIn")
+    : fadeIn(".fadeIn");
+  
   return (
     <>
-      <div data-aos="fade-up" className="robo-set-2">
+      <div ref={sectionRef} className="robo-set-2">
         <div className="mid-heading-text">
-          <div data-aos="fade-left">
+          <div className="fadeIn">
             Designs that <br />
             <label className="mid-heading-span" data-text="make a difference.">
               make a difference.
             </label>
           </div>
         </div>
-        <img src={robo} alt="robo" className="robo2" />
+        <img src={robo} alt="robo" className="robo2 fadeIn" />
       </div>
     </>
   );
