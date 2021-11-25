5603f2cb37a00d00db999fe9781284ef03b6d605
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-25 18:12:04 +0530

    galery animation removed

--- src/Components/SliderGallery.js
+++ src/Components/SliderGallery.js
@@ -1,18 +1,10 @@
 import React, { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 const SliderGallery = () => {
-  useEffect(() => {
-    AOS.init({
-      offset: 400,
-      duration: 2000,
-      easing: "ease-in",
-    });
-  });
+  
   return (
     <>
-      <div data-aos="fade-in" className="">
+      <div className="">
         <div class="slider">
           <div class="slide-track">
             <div class="slide"></div>
--- src/Components/SliderGalleryReverse.js
+++ src/Components/SliderGalleryReverse.js
@@ -1,21 +1,11 @@
-import React, { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React from "react";
 
 const SliderGalleryReverse = () => {
-  useEffect(() => {
-    AOS.init({
-      offset: 300,
-      duration: 2000,
-      easing: "ease-in",
-      delay: 200,
-    });
-  });
+
   return (
     <>
       <div
         className="slider"
-        data-aos="fade-up"
         style={{ position: "relative", top: "-60px" }}
       >
         <div class="slider">
