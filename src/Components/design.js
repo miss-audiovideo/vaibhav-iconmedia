079047fdd66e394b220b587818d8c8dc58f35d17
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-11-25 19:15:11 +0530

    clean animation

--- src/Components/design.js
+++ src/Components/design.js
@@ -4,43 +4,34 @@ import gsap from "gsap";
 
 
 const Design = () => {
-  const sectionRef = useRef(null);
+  const roboRef = useRef(null);
+  const textRef = useRef(null);
   // All the ref to be observed
-  const intersection = useIntersection(sectionRef, {
+  const intersection = useIntersection(roboRef, {
     root: null,
-    rootMargin: "0px",
+    rootMargin: "-280px",
+    threshold: 0.2
+  });
+  const intersection2 = useIntersection(textRef, {
+    root: null,
+    rootMargin: "-180px",
     threshold: 0.2
   });
 
   // Animation for fading in
-  const fadeIn = element => {
-    gsap.to(element, 1, {
-      opacity: 1,
-      y: -60,
-      ease: "slow(0.7, 0.7, false)",
-      stagger: {
-        amount: 0.3
-      }
-    });
-  };
+  const fadeIn = el => {gsap.to(el, .8, {opacity: 1,y: -60,ease: "expo.out",}, 0.2); };
   // Animation for fading out
-  const fadeOut = element => {
-    gsap.to(element, 1, {
-      opacity: 0,
-      y: -20,
-      ease: "slow(0.7, 0.7, false)",
-    });
-  };
+  const fadeOut = el => { gsap.to(el, .8, {opacity: 0, y: -40, ease: "expo.out",}, 0.2); };
 
   // checking to see when the vieport is visible to the user
-  intersection && intersection.intersectionRatio < 0.2
+  intersection && intersection2 && intersection2.intersectionRatio && intersection.intersectionRatio < 0.2
     ? fadeOut(".fadeIn")
     : fadeIn(".fadeIn");
     
   return (
-    <div ref={sectionRef} className="D-container fadeIn">
-      <div className="D-left"></div>
-      <div className="D-right">
+    <div className="D-container fadeIn">
+      <div ref={roboRef} className="D-left"></div>
+      <div ref={textRef} className="D-right">
         <h1>great design</h1>
         <h3>get you noticed.</h3>
         <h1>it's that simple</h1>
